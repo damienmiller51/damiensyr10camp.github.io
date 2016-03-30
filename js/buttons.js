27e46778a2bb8fd07382d563ce06f0136c69d2ab
mtaspiringweb <damienmiller51@gmail.com>
2016-03-30 13:19:22 +1300

    Added files via upload

---
+++ js/buttons.js
@@ -0,0 +1,18 @@
+function switchToHome() {
+location.href = "index.html";
+}
+
+
+function switchToActivities() {
+location.href = "activities.html";
+}
+
+
+function switchToHistory() {
+location.href = "history.html";
+}
+
+
+function switchToInfoPg() {
+location.href = "info.html";
+}
